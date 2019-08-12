2c6413829ab07a7183fa71b7c5a04908555abb1c
Merelvanpuymbroeck <Merelvanpuymbroeck@gmail.com>
2019-08-12 14:06:05 +0200

    selection of table

--- index.php
+++ index.php
@@ -9,6 +9,27 @@ if ($conn->connect_error) {
 } 
 echo "Connected successfully";
 
+$json = "{
+    'id': '$idClean',
+    'title': '$titleClean',
+    'content': '$contentClean',
+    'made_at': '2019-08-12'
+}";
+
+$jsonData =json_encode($json);
+
+//Select table
+$mysql= "INSERT INTO notes (id, title, content, made_at)
+            VALUES ('$titleClean', '$contentClean','2019-08-12')";
+$result = $conn->query($jsonData);
+
+if ($result) {
+    echo "$json";
+} else {
+    echo "Error: " . $mysql . "<br>" . $conn->error;
+}
+
+$conn->close;
 $title = $_GET['title'];
 $text = $_GET ['text'];
 $note = $_POST['note'];
